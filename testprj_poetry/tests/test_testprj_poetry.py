511a155433ab13bc2255ddc95f8c4eff5b779106
Muhammad Saleh Saqib <salehsaqib@gmail.com>
2024-08-09 14:02:27 +0500

    poetry test github

---
+++ testprj_poetry/tests/test_testprj_poetry.py
@@ -0,0 +1,9 @@
+from testprj_poetry import main
+
+def test_function1():
+    r = main.my_first_function()
+    assert r == "Hello World"
+
+def test_function2():
+    r = main.my_first_function()
+    assert r != "Pakistan"
